e9718fd7f3ff576f41db61b6739ab6b8136f7ec7
tahmedofficial <tanvirofcuo@gmail.com>
2024-09-17 19:15:27 +0600

    finish banner and project set up

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './Routes/Routes.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <div className='max-w-7xl mx-auto'>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  </div>
+)
